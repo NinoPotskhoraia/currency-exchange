5caaee1444610006344e39176d5444ef9a81efb9
Nino Potskhoraia <86974590+NinoPotskhoraia@users.noreply.github.com>
2023-01-10 16:00:17 +0400

    added input specifiers

--- currency-exchange/src/app/features/components/currency-front/currency-front.component.ts
+++ currency-exchange/src/app/features/components/currency-front/currency-front.component.ts
@@ -17,6 +17,25 @@ export class CurrencyFrontComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.registerValueChanges();
+  }
+
+  public registerValueChanges(){
+
+  }
+
+
+  get currency1(): FormControl {
+    return this.form.get('currency1') as FormControl;
+  }
+  get amount1(): FormControl {
+    return this.form.get('amount1') as FormControl;
+  }
+  get currency2(): FormControl {
+    return this.form.get('currency2') as FormControl;
+  }
+  get amount2(): FormControl {
+    return this.form.get('amount2') as FormControl;
   }
 
 }
